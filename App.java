90043e73a1c8584b53150277375cadb46b2356ea
Fabio <fabiodtoguchi@gmail.com>
2023-12-05 16:40:04 -0500

    Commit

--- App.java
+++ App.java
@@ -1,7 +1,7 @@
 public class App{
     public static void main(String[] args) {
-        System.out.println("Miles       Kilometers");
-        System.out.println("----------------------");
+        System.out.println("Miles" + "\t\t" + "Kilometers");
+        System.out.println("----------------------------");
         for (int miles=1;miles<=10;miles++) {
             double kilometers = miles*1.609;
             System.out.println(miles + "mi." + "\t\t" + kilometers + "km.");
