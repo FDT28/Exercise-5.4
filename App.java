e6292e77cef0cdca35572fdfe547dc00375f8623
Fabio <fabiodtoguchi@gmail.com>
2023-12-05 16:38:37 -0500

    Commmit

--- App.java
+++ App.java
@@ -4,7 +4,7 @@ public class App{
         System.out.println("----------------------");
         for (int miles=1;miles<=10;miles++) {
             double kilometers = miles*1.609;
-            System.out.println(miles + "mi." + ".\t" + kilometers + "km.");
+            System.out.println(miles + "mi." + "\t" + kilometers + "km.");
         }
     }
 }
