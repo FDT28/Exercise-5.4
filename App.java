dbbe8dcbdbcdde7e2fa98371f4396b2a381fa166
Fabio <fabiodtoguchi@gmail.com>
2023-12-05 16:18:32 -0500

    commit

--- App.java
+++ App.java
@@ -1,13 +1,9 @@
 public class App{
     public static void main(String[] args) {
-        // Display the table header
-        System.out.println("Miles\tKilometers");
-        System.out.println("--------------------");
-
-        // Display the conversion table for miles 1 to 10
-        for (int miles = 1; miles <= 10; miles++) {
-            double kilometers = miles * 1.609;
-            System.out.printf("%d\t%.3f%n", miles, kilometers);
+        System.out.println("Miles       Kilometers");
+        System.out.println("----------------------");
+        for (int miles=1;miles<=10;miles++) {
+            System.out.println("miles");
         }
     }
-}   
+}
