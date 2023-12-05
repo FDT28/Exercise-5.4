4ddf8aa9d502e73215f1639b8a2bf51498df5fd8
Fabio <fabiodtoguchi@gmail.com>
2023-12-05 16:36:18 -0500

    Test

--- App.java
+++ App.java
@@ -1,10 +1,13 @@
-public class App{
+public class App {
     public static void main(String[] args) {
-        System.out.println("Miles       Kilometers");
-        System.out.println("----------------------");
-        for (int miles=1;miles<=10;miles++) {
-            double kilometers = miles*1.609;
-            System.out.println(miles + "mi." + "       " + kilometers + "km.");
+        // Display the table header
+        System.out.println("Miles\tKilometers");
+        System.out.println("--------------------");
+
+        // Display the conversion table for miles 1 to 10
+        for (int miles = 1; miles <= 10; miles++) {
+            double kilometers = miles * 1.609;
+            System.out.printf("%d\t%.3f%n", miles, kilometers);
         }
     }
 }
